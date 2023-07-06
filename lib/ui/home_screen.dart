3332f6a08dc7aee32c0825bf4d1241a4c90d989f
ohsangsoo <tkdtn3264@gmail.com>
2023-07-06 16:58:28 +0900

    이미지검색 앱 검색창 모서리을 10.0 둥글게 만든다.

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -13,9 +13,19 @@ class HomeScreen extends StatelessWidget {
           style: TextStyle(color: Colors.black), //제목 이름 검은색으로
         ),
         backgroundColor: Colors.white, // 배경 색을 흰색으로
-        elevation: 0, // elevation그림자를 0으다
+        elevation: 0, // elevation그림자를 0 으로한다
       ),
-      body: Container(),
+      body: Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0),) // 검색창 모서리 각도 10 둥글게 한다.
+            )
+          ),
+        )
+      ],
+      ),
     );
   }
 }
