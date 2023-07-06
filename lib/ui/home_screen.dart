612956c120888c70ff6b0d5a91e2bb50ad3c22aa
ohsangsoo <tkdtn3264@gmail.com>
2023-07-06 17:26:45 +0900

    const 는 맨 마지막 에 해야 중간 중간 로직 생성 할때 오류가 없다.

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -5,26 +5,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         centerTitle: true, // 제목을 가운데로 오게한다.
         title: const Text(
-            '이미지 검색 앱',
+          '이미지 검색 앱',
           style: TextStyle(color: Colors.black), //제목 이름 검은색으로
         ),
         backgroundColor: Colors.white, // 배경 색을 흰색으로
         elevation: 0, // elevation그림자를 0 으로한다
       ),
-      body: Column(
-      children: [
-        TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10.0),) // 검색창 모서리 각도 10 둥글게 한다.
+      body:  Padding(  //const  컴파일 타임에 만들어지고 계속계속해서 재사용 되서 메모리를 줄여주는 효과 가 있다.
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(10.0)), // 검색창 모서리 각도 10 둥글게 한다.
+                ),
+                suffixIcon: IconButton(onPressed: () {
+                  // 아이콘
+                }, icon: Icon(Icons.search)
+                ),
+              ),
             )
-          ),
-        )
-      ],
+          ],
+        ),
       ),
     );
   }
