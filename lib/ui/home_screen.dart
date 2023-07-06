42c63f34bc9c32413144c854c6a94d65934cbf3a
ohsangsoo <tkdtn3264@gmail.com>
2023-07-06 16:44:44 +0900

    centerTitle: true :제목을 가운데로 오게한다. style: TextStyle(color: Colors.black):제목 이름 검은색으로 한다 backgroundColor: Colors.white, 배경 색을 흰색으로한다   elevation: 0, // elevation그림자를 0이다.

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -7,7 +7,13 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: const Text('이미지검색 앱'),
+        centerTitle: true, // 제목을 가운데로 오게한다.
+        title: const Text(
+            '이미지 검색 앱',
+          style: TextStyle(color: Colors.black), //제목 이름 검은색으로
+        ),
+        backgroundColor: Colors.white, // 배경 색을 흰색으로
+        elevation: 0, // elevation그림자를 0으다
       ),
       body: Container(),
     );
