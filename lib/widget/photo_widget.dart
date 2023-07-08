fed6a42316bac918fe175fc7a6c82915d529e8b8
ohsangsoo <tkdtn3264@gmail.com>
2023-07-08 14:58:50 +0900

    포토동작하기 위해서 선언후 생성자 추가하기

--- lib/widget/photo_widget.dart
+++ lib/widget/photo_widget.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:image_search/model/Photo.dart';
 
 class PhotoWidget extends StatelessWidget {
-  const PhotoWidget({Key? key}) : super(key: key);
+  final Photo photo; // 포토 필드 전역변수 선언 포토기반 으로 동작 하기위해서
+
+  const PhotoWidget({Key? key, required this.photo})
+      : super(key: key); // this.photo 추가
   @override
   Widget build(BuildContext context) {
-    return Container(decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(16.0)),
-        image: DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage('https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90')
-        )
-    ),
-
+    return Container(
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                  'https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90'))),
     );
   }
 }
