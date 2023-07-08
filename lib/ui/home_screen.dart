8970d80d435548623fa3c86fe593f00c573376ee
ohsangsoo <tkdtn3264@gmail.com>
2023-07-08 16:34:16 +0900

    setState울 사용해야되서  Stateless 로변경

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,24 +1,30 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:image_search/model/Photo.dart';
 import 'package:image_search/widget/photo_widget.dart';
 import 'package:http/http.dart' as http;
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget { // setState울 사용해야되서  Stateless을
   const HomeScreen({Key? key}) : super(key: key);
 
-  // http 통신 비동기
-  Future<List<Photo>> fetcth(String query) async {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  //이런 값을 사용하기 때문에 위에 const 를 사용하면 안된다.
+  final List<Photo> photos = []; // 빈 리스트 초기화  아래 데이터 를 photos 빈 리스트에 채워진다.
+  Future<List<Photo>> fetch(String query) async {
     // 스트링 커리 지정 async  비동기
     final response = await http.get(Uri.parse(
         'https://pixabay.com/api/?key=38081108-118a6127b8642576a388e6c5e&q=$query&image_type=photo&pretty=true'));
     //Response body를 제이슨 디코드변환 사용해야한다.
-    Map<String,  dynamic> jsonResponse = jsonDecode(response.body); //맵형태로 얻는 데이터는 제이슨 형태로 데이터를 얻을수있다
-     // 히즈 문제
+    Map<String, dynamic> jsonResponse =
+        jsonDecode(response.body); //맵형태로 얻는 데이터는 제이슨 형태로 데이터를 얻을수있다
+    // 히즈
     Iterable hits = jsonResponse['hits']; // 현 데이터는 맵형태
-    hits.map((e) => Photo.fromJson(e)).toList(); //map data -> 리스트로 변환
-
+    return hits.map((e) => Photo.fromJson(e)).toList(); //map data -> 리스트로 변환
   }
 
   @override
