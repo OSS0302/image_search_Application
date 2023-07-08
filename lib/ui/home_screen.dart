45f8f6cb10c07fa1c143472b54a7df83c9d5afaf
ohsangsoo <tkdtn3264@gmail.com>
2023-07-08 16:17:17 +0900

    http 비동기 통신

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,8 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:image_search/model/Photo.dart';
 import 'package:image_search/widget/photo_widget.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  // http 통신 비동기
+  Future<List<Photo>> fetcth(String query) async {
+    // 스트링 커리 지정 async  비동기
+    final response = await http.get(Uri.parse(
+        'https://pixabay.com/api/?key=38081108-118a6127b8642576a388e6c5e&q=$query&image_type=photo&pretty=true'));
+    //Response body를 제이슨 디코드변환 사용해야한다.
+    Map<String,  dynamic> jsonResponse = jsonDecode(response.body); //맵형태로 얻는 데이터는 제이슨 형태로 데이터를 얻을수있다
+     // 히즈 문제
+    Iterable hits = jsonResponse['hits']; // 현 데이터는 맵형태
+    hits.map((e) => Photo.fromJson(e)).toList(); //map data -> 리스트로 변환
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,14 +57,16 @@ class HomeScreen extends StatelessWidget {
               child: GridView.builder(
                 padding: const EdgeInsets.all(16.0),
                 itemCount: 10, // 아이템 갯수
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2, //crossAxisCount 열
-                      crossAxisSpacing: 16.0, // 검색 앱 가로세로 세팅
-                      mainAxisSpacing: 16 //  검색앱 가로세로 세팅
-                      ),
-                  itemBuilder: (context, index) {
-                    return const PhotoWidget(); //Photowidget 으로 변경 해주고 호출
-                  },
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, //crossAxisCount 열
+                    crossAxisSpacing: 16.0, // 검색 앱 가로세로 세팅
+                    mainAxisSpacing: 16 //  검색앱 가로세로 세팅
+                    ),
+                itemBuilder: (context, index) {
+                  return const PhotoWidget(
+                    photo: Photo(),
+                  ); //Photowidget 으로 변경 해주고 호출
+                },
               ),
             )
           ],
