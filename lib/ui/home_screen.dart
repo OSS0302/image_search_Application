5191a80f53f1629303c7d1a2fbb24686254b69af
ohsangsoo <tkdtn3264@gmail.com>
2023-07-08 16:48:29 +0900

    검색 컨트롤러 생성

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -12,9 +12,10 @@ class HomeScreen extends StatefulWidget { // setState울 사용해야되서  Sta
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _controller =TextEditingController(); //검색 했을떄 가져올 데이털 컨트롨러 (작성한값을 얻으려고한다) 로직 생성
 
   //이런 값을 사용하기 때문에 위에 const 를 사용하면 안된다.
-  final List<Photo> photos = []; // 빈 리스트 초기화  아래 데이터 를 photos 빈 리스트에 채워진다.
+  final List<Photo> _photos = []; // 빈 리스트 초기화  아래 데이터 를 photos 빈 리스트에 채워진다.
   Future<List<Photo>> fetch(String query) async {
     // 스트링 커리 지정 async  비동기
     final response = await http.get(Uri.parse(
@@ -25,6 +26,12 @@ class _HomeScreenState extends State<HomeScreen> {
     // 히즈
     Iterable hits = jsonResponse['hits']; // 현 데이터는 맵형태
     return hits.map((e) => Photo.fromJson(e)).toList(); //map data -> 리스트로 변환
+  }
+  // 사용후 헤제
+  @override
+  void dispose() {
+    _controller.dispose(); // _controller.dispose 호출
+    super.dispose();
   }
 
   @override
@@ -45,15 +52,19 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(16.0),
               child: TextField(
+                controller: _controller, // TextField 안에 컨트롤러 꽂는다.
                 decoration: InputDecoration(
                   border: const OutlineInputBorder(
                     borderRadius: BorderRadius.all(
                         Radius.circular(10.0)), // 검색창 모서리 각도 10 둥글게 한다.
                   ),
                   suffixIcon: IconButton(
-                    onPressed: () {},
+                    onPressed: () async { // fecth 로직이 Future이기 떄문에 async 사용한다.
+                     final photos =fetch( _controller.text); // 컨트롤러 텍스트를 가져와서 위에 fecth 로직을 수행해라
+
+                    },
                     icon: const Icon(Icons.search),
                   ),
                 ),
