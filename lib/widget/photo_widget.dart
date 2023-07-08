14045eb9cf3bc39004b1c8535a1d4c89b8c4f98f
ohsangsoo <tkdtn3264@gmail.com>
2023-07-08 15:48:44 +0900

    photo.previewURL을 값이 변동 되기때문에 const 나중에 작성 해야된다.

--- lib/widget/photo_widget.dart
+++ lib/widget/photo_widget.dart
@@ -9,12 +9,14 @@ class PhotoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(16.0)),
-          image: DecorationImage(
-              fit: BoxFit.cover,
-              image: NetworkImage(
-                  'https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90'))),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+              photo.previewURL),
+        ),
+      ),
     );
   }
 }
