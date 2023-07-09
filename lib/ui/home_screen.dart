06fb9b007728b06101717ca73a284a1129ba2b4b
ohsangsoo <tkdtn3264@gmail.com>
2023-07-09 18:12:18 +0900

    비동기통신 포토 로직 -> api으로 분리하기

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -15,17 +15,8 @@ class _HomeScreenState extends State<HomeScreen> {
   final _controller =TextEditingController(); //검색 했을떄 가져올 데이털 컨트롨러 (작성한값을 얻으려고한다) 로직 생성
 
    List<Photo> _photos = []; // 빈 리스트 초기화  아래 데이터 를 photos 빈 리스트에 채워진다.
-  Future<List<Photo>> fetch(String query) async {
-    // 스트링 커리 지정 async  비동기
-    final response = await http.get(Uri.parse(
-        'https://pixabay.com/api/?key=38081108-118a6127b8642576a388e6c5e&q=$query&image_type=photo&pretty=true'));
-    //Response body를 제이슨 디코드변환 사용해야한다.
-    Map<String, dynamic> jsonResponse =
-        jsonDecode(response.body); //맵형태로 얻는 데이터는 제이슨 형태로 데이터를 얻을수있다
-    // 히즈
-    Iterable hits = jsonResponse['hits']; // 현 데이터는 맵형태
-    return hits.map((e) => Photo.fromJson(e)).toList(); //map data -> 리스트로 변환
-  }
+
+
   // 사용후 헤제
   @override
   void dispose() {
