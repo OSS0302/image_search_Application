f65aafd03643b12f907df7bbf6260dfb831f1219
ohsangsoo <tkdtn3264@gmail.com>
2023-07-10 14:54:46 +0900

    inheritedwidget을 사용하기위해서 지운다.

--- lib/widget/photo_widget.dart
+++ lib/widget/photo_widget.dart
@@ -5,11 +5,9 @@ import '../data/api.dart';
 
 class PhotoWidget extends StatelessWidget {
   final Photo photo; // 포토 필드 전역변수 선언 포토기반 으로 동작 하기위해서
-  final PixabayApi api; // api 선언
 
   const PhotoWidget({Key? key,
-    required this.photo,
-    required this.api}) // api 샹성자 추가
+    required this.photo}) // api 샹성자 추
       : super(key: key); // this.photo 추가
   @override
   Widget build(BuildContext context) {
