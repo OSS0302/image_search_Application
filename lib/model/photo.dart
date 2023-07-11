83f36642024fc802072cabbb5760c6ddd2f11a06
ohsangsoo <tkdtn3264@gmail.com>
2023-07-11 17:24:29 +0900

    원하는 이름으로 변경할 수있다.

--- lib/model/photo.dart
+++ lib/model/photo.dart
@@ -4,12 +4,15 @@ part 'photo.g.dart';
 class Photo {
   int id; // 아이디
   String tags; // 태그
-  String previewURL; // 프리뷰
+
+  @JsonKey(name: 'previewURL') // 이름이name: 'previewURL  데이터 가 들어왔을떄 맵핑를 하겠다.
+  String previewUrl; // 프리뷰
+
 // 플러터 형식으로 변환하기
   Photo({
     required this.id,
     required this.tags,
-    required this.previewURL
+    required this.previewUrl
   }); //생성자
 
   // 팩토리 추가하기
