95c6d43dfead8f7da039b6db7237bf8a39ca29ce
ohsangsoo <tkdtn3264@gmail.com>
2023-07-11 17:59:29 +0900

    프리포스 추가하기

--- lib/model/photo.dart
+++ lib/model/photo.dart
@@ -1,12 +1,13 @@
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 part 'photo.g.dart';
 @JsonSerializable()
-class Photo {
-  int id; // 아이디
-  String tags; // 태그
+class Photo extends Equatable{ // Equatable 상속 받으면 불변 객체로 봐야한다.
+  final int id; // 아이디
+  final String tags; // 태그
 
   @JsonKey(name: 'previewURL') // 이름이name: 'previewURL  데이터 가 들어왔을떄 맵핑를 하겠다.
-  String previewUrl; // 프리뷰
+  final String previewUrl; // 프리뷰
 
 // 플러터 형식으로 변환하기
   Photo({
@@ -19,4 +20,8 @@ class Photo {
   factory Photo.fromJson(Map<String,dynamic> json )=> _$PhotoFromJson(json);
 
   Map<String, dynamic> toJson() => _$PhotoToJson(this);
+
+
+  @override
+  List<Object?> get props => [id];
 }
