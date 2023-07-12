9be004a567264cab0e501d3f4a4af6cd101b568d
ohsangsoo <tkdtn3264@gmail.com>
2023-07-12 14:55:12 +0900

    freezed 라이브 템플릿 만들어서 코드 작성

--- lib/model/photo.dart
+++ lib/model/photo.dart
@@ -1,27 +1,18 @@
-import 'package:equatable/equatable.dart';
+
 import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'photo.freezed.dart';
+
 part 'photo.g.dart';
-@JsonSerializable()
-class Photo extends Equatable{ // Equatable 상속 받으면 불변 객체로 봐야한다.
-  final int id; // 아이디
-  final String tags; // 태그
 
-  @JsonKey(name: 'previewURL') // 이름이name: 'previewURL  데이터 가 들어왔을떄 맵핑를 하겠다.
-  final String previewUrl; // 프리뷰
+@freezed
+class photo with _$photo {
+  factory photo({
+   required int id,
+   required String tags,
+   required String previewURL,
+  }) = _photo;
 
-// 플러터 형식으로 변환하기
-   Photo({
-    required this.id,
-    required this.tags,
-    required this.previewUrl
-  }); //생성자
-
-  // 팩토리 추가하기
-  factory Photo.fromJson(Map<String,dynamic> json )=> _$PhotoFromJson(json);
-
-  Map<String, dynamic> toJson() => _$PhotoToJson(this);
-
-// == hashcode 와 toString 를 한 로직 과 같다
-  @override
-  List<Object?> get props => [id];
+  factory photo.fromJson(Map<String, dynamic> json) => _$photoFromJson(json);
 }
