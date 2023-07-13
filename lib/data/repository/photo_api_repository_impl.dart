c34c474d53a0092c0304c514ead58b6150bbdc03
ohsangsoo <tkdtn3264@gmail.com>
2023-07-13 17:46:21 +0900

    when 를 사용해서 성공시 iterable 맵 데이터를 리스트로 변환해서 반환하고 실패시 메시지를 반환해라

--- lib/data/repository/photo_api_repository_impl.dart
+++ lib/data/repository/photo_api_repository_impl.dart
@@ -8,13 +8,19 @@ class PhotoApiRepositoryImpl implements PhotoApiRepository {
 
   PhotoApiRepositoryImpl(this.api);
 
-
   @override
-  Future <Result<List<Photo>>> fetch(String query) async {
-    final result = await api.fetch(query);
-    result.when(success: success, error: error); // 하나라도 사용안하면 오류가난다. 개발자 실수를 줄인다.
+  Future<Result<List<Photo>>> fetch(String query) async {
+    final Result<Iterable> result = await api.fetch(query);
+
+    return result.when(// 하나라도 사용안하면 오류가난다. 개발자 실수를 줄인다.
+        success: (iterable) { //성공시
+          return Result.success(iterable.map((e) => Photo.fromJson(e)).toList()); //map data -> 리스트로 변환
+        },
+        error: (message) { // 실패시
+          return Result.error(message);
+        });
     // 에러일수도 있고 아닐수도있다.
 
-    return result.map((e) => Photo.fromJson(e)).toList(); //map data -> 리스트로 변환
+
   }
 }
