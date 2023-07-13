9eefc7ae4bd88032ddbfea1ef5d9ad3e6b0df09a
ohsangsoo <tkdtn3264@gmail.com>
2023-07-13 17:37:11 +0900

    freezed 를 사용하면 쉽게 에러와 성공를 쓸수있다.

--- lib/data/repository/photo_api_repository_impl.dart
+++ lib/data/repository/photo_api_repository_impl.dart
@@ -11,8 +11,8 @@ class PhotoApiRepositoryImpl implements PhotoApiRepository {
 
   @override
   Future <Result<List<Photo>>> fetch(String query) async {
-
     final result = await api.fetch(query);
+    result.when(success: success, error: error); // 하나라도 사용안하면 오류가난다. 개발자 실수를 줄인다.
     // 에러일수도 있고 아닐수도있다.
 
     return result.map((e) => Photo.fromJson(e)).toList(); //map data -> 리스트로 변환
