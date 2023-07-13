1f39c452250cbd362b0c3494b2120dc700287d3f
ohsangsoo <tkdtn3264@gmail.com>
2023-07-13 16:28:23 +0900

    `http.Client 가 PixabayApi 에 들어가고 PixabayApi가  에들어가고 PhotoApiRepositoryImpl가`
    `HomeViewModel들어가는 구조다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:image_search/data/data_source/Pixabay_api.dart';
 import 'package:image_search/data/photo_provider.dart';
 import 'package:image_search/presentation/home/home_screen.dart';
 import 'package:image_search/presentation/home/home_view_model.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 import 'data/repository/photo_api_repository_impl.dart';
 
@@ -23,7 +25,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: ChangeNotifierProvider ( // 감지할 수있는 프로바이더로
-          create: (_)=> HomeViewModel(PhotoApiRepositoryImpl()),
+          create: (_)=> HomeViewModel(PhotoApiRepositoryImpl(PixabayApi(http.Client()))), // http.Client 가 PixabayApi 에 들어가고 PixabayApi가  에들어가는 구조 다
+
           child: const HomeScreen()
       ),
     );
