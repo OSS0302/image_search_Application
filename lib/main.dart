66364f9a985a310ef119671f1cbf2b1ae3ea95b2
ohsangsoo <tkdtn3264@gmail.com>
2023-07-14 17:56:37 +0900

    멀티 프로바이더를 이용해서 로직  home 코드 간결화

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_search/data/data_source/Pixabay_api.dart';
+import 'package:image_search/data/di/provider_setup.dart';
 import 'package:image_search/data/photo_provider.dart';
 import 'package:image_search/domain/use_case/get_photos_use_case.dart';
 import 'package:image_search/presentation/home/home_screen.dart';
@@ -10,7 +11,11 @@ import 'package:http/http.dart' as http;
 import 'data/repository/photo_api_repository_impl.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(providers: globalProviders,
+        child: const MyApp(),
+  ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -25,11 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ChangeNotifierProvider ( // 감지할 수있는 프로바이더로
-          create: (_)=> HomeViewModel (GetPhotosUseCase (PhotoApiRepositoryImpl(PixabayApi(http.Client())))), // http.Client 가 PixabayApi 에 들어가고 PixabayApi가  에들어가는 구조 다
-
-          child: const HomeScreen()
-      ),
+      home: const HomeScreen(),
     );
   }
 }
