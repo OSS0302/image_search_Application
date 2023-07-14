2d24cf57b882a38170d2b15d5d4bb775969927b1
ohsangsoo <tkdtn3264@gmail.com>
2023-07-14 14:34:08 +0900

    home_view_model.dart에서만 사용하도록 로딩 로직 선언

--- lib/presentation/home/home_view_model.dart
+++ lib/presentation/home/home_view_model.dart
@@ -14,6 +14,11 @@ class HomeViewModel with ChangeNotifier {
   // getter 생성 :내부에서 홈 뷰모델을 고치고 외부에서 _photos를 고치지 못하도록 get 를 사용한다.
   UnmodifiableListView<Photo> get photos => UnmodifiableListView(_photos); //수정 못하는리스트: UnmodifiableListView
 
+  bool _isLoading = false;
+
+
+  bool get isLoading => _isLoading; //getter 를 사용하면 외부에서 사용할수없다 .
+
   // 사용자 한테 에러메시지 를 보여주기위해서 컨트롤러 생성
   final _eventController = StreamController<HomeUiEvent>();
   Stream<HomeUiEvent> get EventStream => _eventController.stream;
@@ -24,14 +29,15 @@ class HomeViewModel with ChangeNotifier {
 
   // _photoStreamController 에 데이터를 채울 메서드를 생성
   Future<void> fetch(String query) async {
+    _isLoading = true;
     final Result<List<Photo>> result = await repository.fetch(query);
     // 검사하기
     result.when(success: (photos) {
         _photos = photos; // 갱신한다.
     }, error: (message) {
         _eventController.add(HomeUiEvent.showSnackBar(message));
-    });
-
-    notifyListeners(); // 감시하는 곳에 알림을 주는 기능
+      }
+    );
+    _isLoading=false;
   }
 }
