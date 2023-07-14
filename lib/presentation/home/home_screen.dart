e34b888f0afc079f65b769158b3cb4d39002b8a7
ohsangsoo <tkdtn3264@gmail.com>
2023-07-14 14:35:36 +0900

    home_screen.dart 에서 삼항연산자를 통해서 뷰모델에서 로딩이면 CircularProgressIndicator() 보여주고 아니면 Expanded를 보여줘라

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -9,16 +9,18 @@ import 'package:provider/provider.dart';
 
 import '../../data/repository/photo_api_repository_impl.dart';
 
-class HomeScreen extends StatefulWidget { // setState울 사용해야되서  Stateless을
-  const HomeScreen({Key? key}) : super(key: key); // final 로 선언해서 required  필요하다.
+class HomeScreen extends StatefulWidget {
+  // setState울 사용해야되서  Stateless을
+  const HomeScreen({Key? key})
+      : super(key: key); // final 로 선언해서 required  필요하다.
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  final _controller =TextEditingController(); //검색 했을떄 가져올 데이털 컨트롨러 (작성한값을 얻으려고한다) 로직 생성
+  final _controller =
+      TextEditingController(); //검색 했을떄 가져올 데이털 컨트롨러 (작성한값을 얻으려고한다) 로직 생성
   StreamSubscription? _streamSubscription;
 
   @override
@@ -26,10 +28,11 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
 
     Future.microtask(() {
-      final viewModel = context.read<HomeViewModel>();// initState안에 뷰모델 사용해야되서 추가
-      _streamSubscription =viewModel.EventStream.listen((event) {
-        event.when(showSnackBar: (message){
-          final snackBar = SnackBar(content:Text(message));
+      final viewModel =
+          context.read<HomeViewModel>(); // initState안에 뷰모델 사용해야되서 추가
+      _streamSubscription = viewModel.EventStream.listen((event) {
+        event.when(showSnackBar: (message) {
+          final snackBar = SnackBar(content: Text(message));
           ScaffoldMessenger.of(context).showSnackBar(snackBar);
         });
       });
@@ -75,33 +78,37 @@ class _HomeScreenState extends State<HomeScreen> {
                         Radius.circular(10.0)), // 검색창 모서리 각도 10 둥글게 한다.
                   ),
                   suffixIcon: IconButton(
-                    onPressed: () async { // fecth 로직이 Future이기 떄문에 async 사용한다.
-                      viewModel.fetch(_controller.text); // 실행하면 photo_privder fetch에 데이터를 넣어준다.
-
-                     },
+                    onPressed: () async {
+                      // fecth 로직이 Future이기 떄문에 async 사용한다.
+                      viewModel.fetch(_controller
+                          .text); // 실행하면 photo_privder fetch에 데이터를 넣어준다.
+                    },
                     icon: const Icon(Icons.search),
                   ),
                 ),
               ),
             ),
-
-            Expanded(
-              child: GridView.builder(
-                padding: const EdgeInsets.all(16.0),
-                itemCount: viewModel.photos.length , //
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, //crossAxisCount 열
-                    crossAxisSpacing: 16.0, // 검색 앱 가로세로 세팅
-                    mainAxisSpacing: 16 //  검색앱 가로세로 세팅
-                ),
-                itemBuilder: (context, index) {
-                  final photo = viewModel.photos[index]; // photo 인덱스 번호 쨰 아이템으로 가져온다.
-                  return PhotoWidget(
-                    photo: photo, // const 위에 있어서 변수를 사용해야되서  오류가 난다.
-                  ); //Photowidget 으로 변경 해주고 호출
-                },
-              ),
-            ),
+            viewModel.isLoading
+                ? CircularProgressIndicator()
+                : Expanded(
+                    child: GridView.builder(
+                      padding: const EdgeInsets.all(16.0),
+                      itemCount: viewModel.photos.length, //
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2, //crossAxisCount 열
+                              crossAxisSpacing: 16.0, // 검색 앱 가로세로 세팅
+                              mainAxisSpacing: 16 //  검색앱 가로세로 세팅
+                              ),
+                      itemBuilder: (context, index) {
+                        final photo = viewModel
+                            .photos[index]; // photo 인덱스 번호 쨰 아이템으로 가져온다.
+                        return PhotoWidget(
+                          photo: photo, // const 위에 있어서 변수를 사용해야되서  오류가 난다.
+                        ); //Photowidget 으로 변경 해주고 호출
+                      },
+                    ),
+                  ),
           ],
         ),
       ),
