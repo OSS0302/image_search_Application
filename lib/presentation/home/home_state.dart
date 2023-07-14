d9b89b6e744221673792d94854a1cf73fa47356d
ohsangsoo <tkdtn3264@gmail.com>
2023-07-14 17:01:49 +0900

    freezed 쉽게 불변 객체 객체 카피해서 값을 변경할수있다.

--- lib/presentation/home/home_state.dart
+++ lib/presentation/home/home_state.dart
@@ -1,12 +1,30 @@
+// import 'package:image_search/domain/model/photo.dart';
+//
+// class HomeState {
+//   final List<Photo> photos;
+//   final bool isLoading;
+//
+//   HomeState(this.photos, this.isLoading); //생성자 추가getter 를 사용하면 외부에서 사용할수없다 .
+//
+//   HomeState copy({List<Photo>? photos, bool? isLoading}) { // 카피하는 메서드 추가
+//     return HomeState(photos ?? this.photos, isLoading ??= this.isLoading);
+//   }
+// }
+
 import 'package:image_search/domain/model/photo.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class HomeState {
-  final List<Photo> photos;
-  final bool isLoading;
+part 'home_state.freezed.dart';
 
-  HomeState(this.photos, this.isLoading); //생성자 추가getter 를 사용하면 외부에서 사용할수없다 .
+part 'home_state.g.dart';
 
-  HomeState copy({List<Photo>? photos, bool? isLoading}) { // 카피하는 메서드 추가
-    return HomeState(photos ?? this.photos, isLoading ??= this.isLoading);
-  }
+@freezed
+class HomeState with _$HomeState {
+  factory HomeState(
+  List<Photo> photos,
+  bool isLoading,
+  ) = _HomeState;
+  
+  factory HomeState.fromJson(Map<String, dynamic> json) => _$HomeStateFromJson(json);
 }
