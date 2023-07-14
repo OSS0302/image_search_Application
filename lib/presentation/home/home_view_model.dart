95e991180a984c864b5cae3567798be5935b3cfa
ohsangsoo <tkdtn3264@gmail.com>
2023-07-14 17:00:48 +0900

    execute -> call 변경

--- lib/presentation/home/home_view_model.dart
+++ lib/presentation/home/home_view_model.dart
@@ -4,11 +4,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:image_search/data/data_source/result.dart';
 import 'package:image_search/domain/repository/photo_api_repository.dart';
 import 'package:image_search/domain/model/photo.dart';
+import 'package:image_search/domain/use_case/get_photos_use_case.dart';
 import 'package:image_search/presentation/home/home_state.dart';
 import 'package:image_search/presentation/home/home_ui_event.dart';
 
 class HomeViewModel with ChangeNotifier {
-  final PhotoApiRepository repository;
+  final GetPhotosUseCase getPhotosUseCase;
   
   HomeState _state = HomeState([], false);
 
@@ -21,23 +22,27 @@ class HomeViewModel with ChangeNotifier {
 
 
 
-  HomeViewModel(this.repository); // 생성자
+  HomeViewModel(this.getPhotosUseCase); // 생성자
+
+  //voidCallback? callback; // funtion 기능이다.
 
   // _photoStreamController 에 데이터를 채울 메서드를 생성
   Future<void> fetch(String query) async {
-    _state = state.copy(isLoading: true); // 기존 state 에서 카피한 로딩이 true 로 한다.
-    state.isLoading = true;
+    //callback(); // call 생략가능하다callback.call();
+
+    _state = state.copyWith(isLoading: true); // 기존 state 에서 카피한 로딩이 true 로 한다.
     notifyListeners(); // 상태 감지
-    final Result<List<Photo>> result = await repository.fetch(query);
+
+      final Result<List<Photo>> result = await getPhotosUseCase(query); // call 는 생략가능해서 getPhotosUseCase.call(qurey);
     // 검사하기
     result.when(success: (photos) {
-      _state = state.copy(photos: photos); // 갱신한다.
+      _state = state.copyWith(photos: photos); // 갱신한다.
       notifyListeners(); // 상태감지
     }, error: (message) {
         _eventController.add(HomeUiEvent.showSnackBar(message));
       }
     );
-    _state = state.copy(isLoading: false);
+    _state = state.copyWith(isLoading: false) ;
     notifyListeners(); // 상태감지
   }
 }
