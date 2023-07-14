f3f71ef235db03eacc2c14ef516a0eaf77e6c76d
ohsangsoo <tkdtn3264@gmail.com>
2023-07-14 17:39:03 +0900

    di 를 provider 통해서 지금 까지 로직 나뉘었다.

--- lib/data/di/provider_setup.dart
+++ lib/data/di/provider_setup.dart
@@ -4,11 +4,15 @@ import 'package:image_search/data/data_source/Pixabay_api.dart';
 import 'package:image_search/data/repository/photo_api_repository_impl.dart';
 import 'package:image_search/domain/repository/photo_api_repository.dart';
 import 'package:image_search/domain/use_case/get_photos_use_case.dart';
+import 'package:image_search/presentation/home/home_view_model.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:http/http.dart'as http;
 // 1. provider 전체
 List<SingleChildWidget> globalProviders =[
+  ...independentModels,
+  ...dependentModels,
+  ...viewModels,
 
 ];
 // 2.독립적인 객체
@@ -32,6 +36,11 @@ List<SingleChildWidget> dependentModels = [
   ProxyProvider<PhotoApiRepository,GetPhotosUseCase>(
       update:(context , repository, _) => GetPhotosUseCase(repository),
   ),
-  //GetPhotosUseCase
 ];
-
+  //GetPhotosUseCase
+  // 4.ViewModels
+  List<SingleChildWidget>viewModels =[
+    ChangeNotifierProvider<HomeViewModel>(
+    create: (context) =>HomeViewModel(context.read<GetPhotosUseCase>()), // context 통해서 getPhotosUseCase 를 받을 수있다.
+    ),
+];
